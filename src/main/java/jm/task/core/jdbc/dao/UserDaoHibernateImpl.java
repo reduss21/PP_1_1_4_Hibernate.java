493bee7c5bedf317b56a4bda85d7e6d42d971a9f
reduss21 <kartosferon@gmail.com>
2023-01-20 15:51:17 +0300

    sessionFactory one and reached

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,6 +5,7 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ public class UserDaoHibernateImpl implements UserDao {
     private static final String GET_ALL_USERS = "from User";
     private static final String CLEAN_USERS_TABLE = "TRUNCATE TABLE User;";
     private Transaction transaction = null;
-    private final Session session = Util.getHibernateSession().openSession();
+    private static final SessionFactory sessionFactory = Util.getHibernateSession();
 
     public UserDaoHibernateImpl() {
 
@@ -26,7 +27,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
-        try {
+        try (Session session = sessionFactory.openSession()){
             transaction = session.beginTransaction();
             session.createSQLQuery(CREATE_USERS_TABLE)
                     .executeUpdate();
@@ -40,7 +41,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        try {
+        try (Session session = sessionFactory.openSession()){
             transaction = session.beginTransaction();
             session.createSQLQuery(DROP_USERS_TABLE)
                     .executeUpdate();
@@ -52,22 +53,26 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try  {
-            transaction = session.beginTransaction();
-            session.save(new User(name, lastName, age));
-            transaction.commit();
-            System.out.printf("User с именем – %s добавлен в базу данных\n", name);
-        } catch (HibernateException e) {
-            System.out.printf("%s - ошибка сохранения пользователя с именем - %s", e.getMessage(), name);
-            if (transaction != null) {
-                transaction.rollback();
+        User user = new User(name, lastName, age);
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = null;
+            try {
+                transaction = session.beginTransaction();
+                session.persist(user);
+                transaction.commit();
+                System.out.printf("User с именем – %s добавлен в базу данных\n", name);
+            } catch (Exception e) {
+                System.out.printf("%s - ошибка сохранения пользователя с именем - %s", e.getMessage(), name);
+                if (transaction != null) transaction.rollback();
+                e.printStackTrace();
             }
         }
     }
 
+
     @Override
     public void removeUserById(long id) {
-        try  {
+        try  (Session session = sessionFactory.openSession()){
             transaction = session.beginTransaction();
             session.delete(session.get(User.class, id));
             transaction.commit();
@@ -84,7 +89,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try  {
+        try  (Session session = sessionFactory.openSession()){
             transaction = session.beginTransaction();
             users = session.createQuery(GET_ALL_USERS).list();
             transaction.commit();
@@ -96,7 +101,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        try  {
+        try  (Session session = sessionFactory.openSession()){
             transaction = session.beginTransaction();
             session.createSQLQuery(CLEAN_USERS_TABLE)
                     .executeUpdate();
